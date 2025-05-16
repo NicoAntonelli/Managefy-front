968615e402a6f077625989071c27f8e6d973c4e6
NicoAntonelli <niconelli2@gmail.com>
2025-05-16 20:34:06 -0300

    Fix splash logo text and add twinkle effect

--- components/Common/Loader/SplashLogo.tsx
+++ components/Common/Loader/SplashLogo.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+const twinkleKeyframes = `
+@keyframes twinkle {
+  0%, 100% { opacity: 1; }
+  50% { opacity: 0.5; }
+}
+`
+
 const SplashLogo = () => {
     return (
         <div
+            id="splash-logo"
             style={{
                 height: '100vh',
                 display: 'flex',
@@ -13,22 +21,34 @@ const SplashLogo = () => {
                 background:
                     'radial-gradient(circle,rgba(255, 255, 255, 1) 0%, rgba(253, 210, 193, 1) 50%, rgba(218, 172, 156, 1) 100%)',
             }}>
-            {/* eslint-disable @next/next/no-img-element */}
-            <img
-                style={{ borderRadius: '50%' }}
-                src="/android-chrome-512x512.png"
-                alt="Managefy"
-                width={200}
-                height={200}
-            />
-            <h1
+            <style>{twinkleKeyframes}</style>
+            <div
+                id="splash-logo-content"
                 style={{
-                    fontFamily: 'Roboto',
-                    letterSpacing: '0.2rem',
-                    color: '#fdd2c1',
+                    textAlign: 'center',
+                    animation: 'twinkle 1.2s infinite ease-in-out',
                 }}>
-                Managefy
-            </h1>
+                {/* eslint-disable @next/next/no-img-element */}
+                <img
+                    style={{
+                        borderRadius: '50%',
+                    }}
+                    src="/android-chrome-512x512.png"
+                    alt="Managefy"
+                    width={200}
+                    height={200}
+                />
+                <h1
+                    style={{
+                        paddingTop: '1rem',
+                        fontFamily: 'Roboto',
+                        letterSpacing: '0.1rem',
+                        color: '#16303d',
+                        fontSizeAdjust: '1',
+                    }}>
+                    Managefy
+                </h1>
+            </div>
         </div>
     )
 }
