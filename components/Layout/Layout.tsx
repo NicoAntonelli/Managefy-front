577467404e0810da517d77583206d5d1d19978b2
NicoAntonelli <niconelli2@gmail.com>
2025-05-16 01:54:44 -0300

    Add Layout component and replace current layout

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+import {
+    AppShell,
+    useComputedColorScheme,
+    useMantineColorScheme,
+} from '@mantine/core'
+import { useHotkeys } from '@mantine/hooks'
+
+import Header from './Header/Header'
+import Navbar from './Navbar/Navbar'
+import SplashLogo from '@/components/Common/Loader/SplashLogo'
+import useSidebarStore from '@/utils/stores/useSidebarStore'
+
+interface Layout {
+    children: React.ReactNode
+}
+
+const Layout = (props: Layout) => {
+    const opened = useSidebarStore((state) => state.opened)
+    const pathName = usePathname()
+    const [unloaded, setUnloaded] = useState(true)
+
+    // Change between theme preferences
+    const { setColorScheme } = useMantineColorScheme()
+    const computedColorScheme = useComputedColorScheme('dark')
+    const toggleColorScheme = () =>
+        setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark')
+
+    // Hotkeys
+    useHotkeys([['mod+J', () => toggleColorScheme()]])
+
+    useEffect(() => {
+        setUnloaded(false)
+    }, [])
+
+    const layoutSet = () => {
+        // Unloaded page
+        if (unloaded) return <SplashLogo />
+
+        // Layout
+        return (
+            <AppShell
+                header={{ height: 60 }}
+                navbar={{
+                    width: { base: 270 },
+                    breakpoint: 'sm',
+                    collapsed: { mobile: !opened },
+                }}>
+                <AppShell.Header>
+                    <Header
+                        toggleColorScheme={toggleColorScheme}
+                        showNavbar={true}
+                    />
+                </AppShell.Header>
+                <AppShell.Navbar>
+                    <Navbar />
+                </AppShell.Navbar>
+                <AppShell.Main>{props.children}</AppShell.Main>
+            </AppShell>
+        )
+    }
+
+    return layoutSet()
+}
+
+export default Layout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,48 @@
-import type { Metadata } from 'next'
+'use client'
+import React from 'react'
+
 import { Inter } from 'next/font/google'
+
+import { MantineProvider, localStorageColorSchemeManager } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Theme from './theme'
+import Layout from '@/components/Layout/Layout'
 
-export const metadata: Metadata = {
-    title: 'Managefy',
-    description: 'Easy-to-use resource management for your business',
-}
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
+    // Detect the user's theme preference (dark or light)
+    const colorSchemeManager = localStorageColorSchemeManager({
+        key: 'mantine-color-scheme',
+    })
+
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+        <html lang="es">
+            <head>
+                <title>Managefy</title>
+                <meta
+                    name="description"
+                    content="Easy-to-use resource management for your business"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
+            <body className={inter.className}>
+                <MantineProvider
+                    theme={Theme}
+                    colorSchemeManager={colorSchemeManager}
+                    defaultColorScheme="dark">
+                    <Layout>{children}</Layout>
+                </MantineProvider>
+            </body>
         </html>
     )
 }
