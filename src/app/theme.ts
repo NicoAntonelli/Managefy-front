85e6a6f36133f7a28e7c4fcad2c5ae46a1877eb1
NicoAntonelli <niconelli2@gmail.com>
2025-05-16 01:52:21 -0300

    Add Theme

---
+++ src/app/theme.ts
@@ -0,0 +1,22 @@
+import { createTheme } from '@mantine/core'
+
+// Mantine theme
+const Theme = createTheme({
+    primaryColor: 'blue',
+    shadows: {
+        md: '1px 1px 3px rgba(0, 0, 0, .25)',
+        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    },
+    fontFamily: 'Roboto, Verdana, sans-serif',
+    fontFamilyMonospace: 'Monaco, Courier, monospace',
+    headings: { fontFamily: 'Greycliff CF, sans-serif' },
+    breakpoints: {
+        xs: '36rem',
+        sm: '72rem',
+        md: '96rem',
+        lg: '124rem',
+        xl: '150rem',
+    },
+})
+
+export default Theme
