997c7577015d9f15cca04dcc44e499d7ea5ba27a
NicoAntonelli <niconelli2@gmail.com>
2025-06-01 20:12:39 -0300

    Add error components

---
+++ components/Common/Error/ErrorGeneric.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react'
+import Link from 'next/link'
+
+import { Button, Flex, Group, Text } from '@mantine/core'
+import Theme from '@/app/theme'
+
+interface ErrorProps {
+    error: Error & { digest?: string }
+    reset: () => void
+}
+
+const ErrorGeneric = (props: ErrorProps) => {
+    useEffect(() => {
+        console.error(props.error)
+    }, [props.error])
+
+    return (
+        <Flex mt={'2rem'} direction={'column'} align={'center'}>
+            <Text mt={'2rem'} size={'2.5rem'}>
+                MANAGEFY
+            </Text>
+            <Text size={'6rem'}>Error</Text>
+            <Text
+                mt={'2rem'}
+                pt={'2rem'}
+                style={{ borderTop: `1px solid $'orange.4'` }}>
+                Ha ocurrido un problema en la pantalla anterior
+            </Text>
+            <Group mt={'2rem'}>
+                <Link href="/">
+                    <Button color={Theme.primaryColor}>Volver al inicio</Button>
+                </Link>
+            </Group>
+            <Group mt={'2rem'}>
+                <Link href="/help#reportBug">
+                    <Button color="orange.6">Reportar bug</Button>
+                </Link>
+            </Group>
+            <Button mt={'2rem'} color="orange.6" onClick={() => props.reset()}>
+                Intentar nuevamente
+            </Button>
+        </Flex>
+    )
+}
+
+export default ErrorGeneric
---
+++ components/Common/Error/ErrorNotFound.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { Button, Flex, Group, Text } from '@mantine/core'
+import Theme from '@/app/theme'
+
+const ErrorNotFound = () => {
+    return (
+        <Flex mt={'2rem'} direction={'column'} align={'center'}>
+            <Text mt={'2rem'} size={'2.5rem'}>
+                MANAGEFY
+            </Text>
+            <Text size={'8rem'}>404</Text>
+            <Text
+                mt={'2rem'}
+                pt={'2rem'}
+                style={{ borderTop: '1px solid orange' }}>
+                Error: Página o recurso no encontrado
+            </Text>
+            <Group mt={'2rem'}>
+                <Link href="/">
+                    <Button color={Theme.primaryColor}>Volver al inicio</Button>
+                </Link>
+            </Group>
+            <Group mt={'2rem'}>
+                <Link href="/help#reportBug">
+                    <Button color="orange.6">Reportar bug</Button>
+                </Link>
+            </Group>
+        </Flex>
+    )
+}
+
+export default ErrorNotFound
