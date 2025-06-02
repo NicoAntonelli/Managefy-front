19e13ce2497d9348768f21e2a7a40d6bb25d41d0
NicoAntonelli <niconelli2@gmail.com>
2025-06-02 01:46:57 -0300

    Users: fix register endpoint and add login endpoint with session cookie HTTP-only

--- services/user.ts
+++ services/user.ts
@@ -2,18 +2,22 @@ import axios from 'axios'
 import Env from '@/utils/Env'
 import Helper from './helper'
 
+import Login from '@/entities/Login'
 import Registration from '@/entities/Registration'
 import Token from '@/entities/Token'
+import User from '@/entities/User'
 
 const prefix = `${Env.backendAPI}/users`
 
-const register = async (register: Registration): Promise<Token> => {
-    const endpoint = `${prefix}/register`
+const login = async (login: Login): Promise<User> => {
+    const endpoint = `${prefix}/login`
     try {
-        const response = await axios.post<Token>(endpoint, register)
+        const response = await axios.post<Token>(endpoint, login)
         Helper.validateResponse(response)
 
-        return response.data
+        const user = sessionCreate(response.data)
+
+        return user
     } catch (error: any) {
         const errorMessage = `Error fetching ${endpoint}: '${error}'`
         console.error(errorMessage)
@@ -21,5 +25,50 @@ const register = async (register: Registration): Promise<Token> => {
     }
 }
 
-const User = { register }
-export default User
+const register = async (register: Registration): Promise<User> => {
+    const endpoint = `${prefix}/register`
+    try {
+        const response = await axios.post<Token>(endpoint, register)
+        Helper.validateResponse(response)
+
+        const user = sessionCreate(response.data)
+
+        return user
+    } catch (error: any) {
+        const errorMessage = `Error fetching ${endpoint}: '${error}'`
+        console.error(errorMessage)
+        throw new Error(error)
+    }
+}
+
+const sessionCreate = async (token: Token): Promise<User> => {
+    const endpoint = `${Env.baseURL}/api/login`
+    try {
+        if (!token.accessToken) {
+            throw new Error('Access token is missing in the response')
+        }
+
+        const response = await axios.post<string>(endpoint, token)
+        Helper.validateResponse(response)
+
+        //const user:User = deserializeCookie(token.accessToken)
+
+        // Dummy user
+        const user: User = {
+            id: 1,
+            email: 'jdoe@example.com',
+            name: 'John Doe',
+            validated: true,
+            emailNotifications: true,
+        }
+
+        return user
+    } catch (error: any) {
+        const errorMessage = `Error fetching ${endpoint}: '${error}'`
+        console.error(errorMessage)
+        throw new Error(error)
+    }
+}
+
+const Users = { login, register }
+export default Users
