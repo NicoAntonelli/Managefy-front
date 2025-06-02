24ff43092f298e285d921c9ed597f288c5b3dbdc
NicoAntonelli <niconelli2@gmail.com>
2025-06-02 01:42:48 -0300

    Env: add frontend base url

--- utils/Env.ts
+++ utils/Env.ts
@@ -3,6 +3,9 @@
 // Working mode
 const mode: string = process.env.NEXT_PUBLIC_WORKING_MODE ?? 'dev'
 
+// Base frontend URL
+const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+
 // Base backend API
 const backendAPI: string =
     mode == 'prod'
@@ -12,5 +15,5 @@ const backendAPI: string =
 // Managefy contact mail
 const contactMail: string = process.env.NEXT_PUBLIC_CONTACT ?? ''
 
-const Env = { mode, backendAPI, contactMail }
+const Env = { mode, backendAPI, baseURL, contactMail }
 export default Env
