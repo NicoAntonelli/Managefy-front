39bcce48742707158a2c3bab68367c13fa4cb62f
NicoAntonelli <niconelli2@gmail.com>
2025-06-02 01:50:16 -0300

    Users: add logout, getOneUser & sessionGet endpoints

--- services/users.ts
+++ services/users.ts
@@ -9,6 +9,20 @@ import User from '@/entities/User'
 
 const prefix = `${Env.backendAPI}/users`
 
+const getOneUser = async (id: number): Promise<User> => {
+    const endpoint = `${prefix}/${id}`
+    try {
+        const response = await axios.get<User>(endpoint)
+        Helper.validateResponse(response)
+
+        return response.data
+    } catch (error: any) {
+        const errorMessage = `Error fetching ${endpoint}: '${error}'`
+        console.error(errorMessage)
+        throw new Error(error)
+    }
+}
+
 const login = async (login: Login): Promise<User> => {
     const endpoint = `${prefix}/login`
     try {
@@ -16,8 +30,19 @@ const login = async (login: Login): Promise<User> => {
         Helper.validateResponse(response)
 
         const user = sessionCreate(response.data)
-
         return user
+    } catch (error: any) {
+        const errorMessage = `Error fetching ${endpoint}: '${error}'`
+        console.error(errorMessage)
+        throw new Error(error)
+    }
+}
+
+const logout = async (): Promise<void> => {
+    const endpoint = `${Env.baseURL}/api/login`
+    try {
+        const response = await axios.delete<string>(endpoint)
+        Helper.validateResponse(response)
     } catch (error: any) {
         const errorMessage = `Error fetching ${endpoint}: '${error}'`
         console.error(errorMessage)
@@ -70,5 +95,27 @@ const sessionCreate = async (token: Token): Promise<User> => {
     }
 }
 
-const Users = { login, register }
+const sessionGet = async (): Promise<User | null> => {
+    const endpoint = 'test'
+    try {
+        //const user:User = deserializeCookie(token.accessToken)
+
+        // Dummy user
+        const user: User = {
+            id: 1,
+            email: 'jdoe@example.com',
+            name: 'John Doe',
+            validated: true,
+            emailNotifications: true,
+        }
+
+        return user
+    } catch (error: any) {
+        const errorMessage = `Error fetching ${endpoint}: '${error}'`
+        console.error(errorMessage)
+        throw new Error(error)
+    }
+}
+
+const Users = { getOneUser, login, logout, register, sessionGet }
 export default Users
