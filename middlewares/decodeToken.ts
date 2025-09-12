69bb21d6bcab2475712d506abfde7305049700b7
NicoAntonelli <niconelli2@gmail.com>
2025-09-12 01:30:32 -0300

    Add decodeToken middleware

---
+++ middlewares/decodeToken.ts
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken'
+import JWTPayload from '@/entities/helpTypes/JWTPayload'
+import User from '@/entities/User'
+
+// Decode token without signature (validation) and return decoded values
+const decodeToken = (token: string): User | null => {
+    if (!token) return null
+
+    try {
+        // Decode JWT
+        const user: JWTPayload | null = jwt.decode(token) as JWTPayload | null
+        if (!user) throw new Error('Error decoding JWT')
+
+        // Extract key-value pairs from 'sub' string
+        const content = user.sub.replace(/^User{|}$/g, '').split(', ')
+
+        // Create dynamic user object
+        const userObject: any = {}
+        content.forEach((pair: string) => {
+            const [key, rawValue] = pair.split('=')
+            let value: any = rawValue
+
+            // Type casting for non-string values
+            if (value === 'true' || value === 'false') {
+                value = value === 'true'
+            } else if (!isNaN(Number(value))) {
+                value = Number(value)
+            }
+
+            userObject[key] = value
+        })
+
+        return userObject as User
+    } catch (error) {
+        console.log('Error while decoding token:', error)
+        return null
+    }
+}
+
+export default decodeToken
