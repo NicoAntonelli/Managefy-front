05e75b2fe5b878b0b862967edc1285d589176d55
NicoAntonelli <niconelli2@gmail.com>
2025-09-12 01:36:15 -0300

    Add a placeholder business page

---
+++ src/app/businesses/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+const BusinessPage = () => {
+    return (
+        <div className="flex flex-col items-center min-w-full h-screen">
+            <p>User logged!!!</p>
+        </div>
+    )
+}
+
+export default BusinessPage
