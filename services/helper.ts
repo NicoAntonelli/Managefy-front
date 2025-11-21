16afc32650f083bd157eb17242f12f607996d55b
NicoAntonelli <niconelli2@gmail.com>
2025-11-21 00:30:51 -0300

    Error handling: Add type guard clause for ErrorResponse

--- services/helper.ts
+++ services/helper.ts
@@ -1,16 +1,34 @@
 import { AxiosResponse } from 'axios'
+import ErrorResponse from '@/entities/helpTypes/ErrorResponse'
 
+// Type guard to check if data is an ErrorResponse
+const isErrorResponse = (data: any): data is ErrorResponse => {
+    return (
+        data &&
+        typeof data === 'object' &&
+        typeof data.message === 'string' &&
+        typeof data.status === 'number' &&
+        typeof data.error === 'string'
+    )
+}
+
+// Validate standard API response
 const validateResponse = (response: AxiosResponse<any, any>) => {
     if (!response) {
         throw new Error('No response from API')
-    } else if (response.status !== 200) {
+    }
+
+    if (response.status !== 200) {
         throw new Error(
             `API responded with an invalid status: ${response.status} '${response.statusText}'`
         )
-    } else if (!response.data) {
+    }
+
+    if (!response.data) {
         throw new Error('API responded with 200 but with an empty body')
     }
 }
 
-const Helper = { validateResponse }
+const Helper = { isErrorResponse, validateResponse }
+
 export default Helper
