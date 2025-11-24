37479f0de63dad076349018468e945de534a7b45
NicoAntonelli <niconelli2@gmail.com>
2025-11-24 19:56:39 -0300

    Health: standardize unique endpoint

--- services/health.ts
+++ services/health.ts
@@ -5,21 +5,14 @@ import Helper from './helper'
 const prefix = Env.backendAPI
 
 const testAPI = async (): Promise<string> => {
+    const endpoint = prefix
     try {
-        const response = await api.get<string>(prefix)
-        if (!response) {
-            throw new Error('No response from API')
-        } else if (response.status !== 200) {
-            throw new Error(
-                `API responded with an invalid status: ${response.status} '${response.statusText}'`
-            )
-        } else if (!response.data) {
-            throw new Error('API responded with 200 but with an empty body')
-        }
+        const response = await api.get<string>(endpoint)
+        Helper.validateResponseAPI(response)
 
         return response.data
     } catch (error: any) {
-        throw new Error(Helper.parseLogErrorAPI(error, '/'))
+        throw new Error(Helper.parseLogErrorAPI(error, endpoint))
     }
 }
 
