85236a3c20cf32a674f72758da2774cec31676b8
NicoAntonelli <niconelli2@gmail.com>
2025-11-24 23:40:22 -0300

    Add Home component

---
+++ components/Home/Home.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import { useMediaQuery } from '@mantine/hooks'
+import { useRouter } from 'next/navigation'
+
+import Health from '@/services/health'
+import Helper from '@/services/helper'
+import Theme from '@/app/theme'
+
+import LoginRegister from '@/components/User/LoginRegister/LoginRegister'
+import SkeletonFull from '@/components/Common/Loader/SkeletonFull'
+import { Card, Group, Image, Stack, Text } from '@mantine/core'
+
+const getHealth = async () => {
+    try {
+        const health = await Health.testAPI()
+        console.log(health)
+    } catch (error) {
+        console.log("Can't connect to Managefy API right now. Try again later.")
+    }
+}
+
+const Home = () => {
+    const isMobile = useMediaQuery(`(max-width: ${Theme.breakpoints?.lg})`)
+    useEffect(() => {
+        getHealth()
+
+        const fetchUser = async () => {
+            try {
+                const user = await Helper.getUserOrAuthenticate(router, false)
+                if (user?.email) {
+                    if (user.validated) router.push('/businesses')
+                    else router.push('/users/validation')
+                }
+            } catch (error) {
+                // Can't fetch user nor log error, stay on home page
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchUser()
+    }, [])
+
+    const [loading, setLoading] = useState(true)
+
+    const router = useRouter()
+
+    if (loading) {
+        return <SkeletonFull />
+    }
+
+    return (
+        <Stack gap="2rem" align="center">
+            <Card
+                shadow="sm"
+                padding="lg"
+                radius="md"
+                withBorder
+                className={isMobile ? 'min-w-full' : 'w-screen-l'}>
+                <Card.Section p="1rem">
+                    <Group
+                        justify="flex-start"
+                        gap={isMobile ? '1rem' : '2rem'}>
+                        <Image
+                            src="/Managefy-logo.jpeg"
+                            alt="Managefy logo"
+                            w="25%"
+                            radius="md"
+                        />
+                        <Stack gap="0.25rem">
+                            <Text size={isMobile ? '2rem' : '6rem'}>
+                                Managefy
+                            </Text>
+                            <Text size={isMobile ? 'sm' : 'xl'} mt="0.5rem">
+                                <b>
+                                    Gestión de recursos fácil de usar para su
+                                    negocio
+                                </b>
+                            </Text>
+                        </Stack>
+                    </Group>
+                </Card.Section>
+            </Card>
+            <LoginRegister />
+        </Stack>
+    )
+}
+
+export default Home
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-import React from 'react'
-import Health from '@/services/health'
-
-const getHealth = async () => {
-    try {
-        const health = await Health.testAPI()
-        return health
-    } catch (error) {
-        return "Can't connect to Managefy API right now. Try again later."
-    }
-}
+'use client'
+import Home from '@/components/Home/Home'
 
 const HomePage = () => {
     return (
         <main>
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p>{getHealth()}</p>
+            <div className="w-full items-center justify-between font-sans flex">
+                <Home />
             </div>
         </main>
     )
