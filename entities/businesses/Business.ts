c5a690c2e0e815cbc22478b7514ae9b8cd4f3ec9
NicoAntonelli <niconelli2@gmail.com>
2026-08-28 00:51:59 -0300

    Business: update businessDays type to use WeekDay

--- entities/businesses/Business.ts
+++ entities/businesses/Business.ts
@@ -1,10 +1,12 @@
+import WeekDay from '../helpTypes/WeekDay'
+
 interface Business {
     id: number
     name: string
     description: string
     link: string
     isPublic: boolean
-    businessDays: Record<string, boolean>
+    businessDays: Record<WeekDay, boolean>
 }
 
 export default Business
