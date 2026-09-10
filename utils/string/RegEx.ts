917fdb73311e59cc824fa1cfbb15fda73d84a97a
NicoAntonelli <niconelli2@gmail.com>
2026-09-09 22:56:50 -0300

    Utils: add cleanInteger and cleanDecimal methods

--- utils/string/RegEx.ts
+++ utils/string/RegEx.ts
@@ -1,5 +1,17 @@
 //////////// REGEX TOOLS ////////////
 
+// Replace non-digit characters with empty string
+const cleanInteger = (value: string): string => value.replace(/[^\d]/g, '')
+
+// Replace non-digit characters (but ONE point) with empty string and handle non-valid formats
+const cleanDecimal = (value: string): string => {
+    let filtered: string = value.replace(/[^\d.]/g, '')
+    const parts: string[] = filtered.split('.')
+    if (parts.length > 2) filtered = `${parts[0]}.${parts.slice(1).join('')}`
+
+    return filtered
+}
+
 // Email: standard cases validation
 const email: RegExp = new RegExp(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -16,5 +28,5 @@ const phone: RegExp = new RegExp(/^\d{5,18}$/)
 // URL path segment: words separated by single hyphens
 const urlSegment: RegExp = new RegExp(/^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/)
 
-const RegEx = { email, password, phone, urlSegment }
+const RegEx = { cleanDecimal, cleanInteger, email, password, phone, urlSegment }
 export default RegEx
