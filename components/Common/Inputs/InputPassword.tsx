5a0e674fe0561307bac00fcb83f74b6310fef352
NicoAntonelli <niconelli2@gmail.com>
2026-09-10 02:43:47 -0300

    Common: add input password component too

---
+++ components/Common/Inputs/InputPassword.tsx
@@ -0,0 +1,28 @@
+import type { InputHTMLAttributes } from 'react'
+import { PasswordInput } from '@mantine/core'
+import { IconLock } from '@tabler/icons-react'
+
+interface InputPasswordProps {
+    required?: boolean
+    isConfirmation?: boolean
+    InputProps?: Omit<InputHTMLAttributes<HTMLInputElement>, 'size'>
+}
+
+const InputPassword = (props: InputPasswordProps) => {
+    const { isConfirmation, InputProps } = props
+
+    return (
+        <PasswordInput
+            pt="1rem"
+            required
+            withAsterisk
+            type="password"
+            label="Contraseña"
+            placeholder={isConfirmation ? 'Confirmar contraseña' : 'Contraseña'}
+            leftSection={<IconLock />}
+            {...InputProps}
+        />
+    )
+}
+
+export default InputPassword
